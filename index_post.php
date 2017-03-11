d9aa2c4cb25253b29a35f9d07ce3c76fb3e4d4cf
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 14:08:07 -0400

    add post main

---
+++ index_post.php
@@ -0,0 +1,7 @@
+		<article class="destacada">
+		<style type="text/css" media="screen">.destacada{background-image: url("http://ximg.es/1180x450/ddd/fff.jpg");}</style>
+ 				<span class="text">
+					<span class="tit" title="">Titulo 1</span><br><br>
+					<span class="cont" title="">bla bla bla bla bla bla bla blaasf bla bla bla bla bla bla bla bla bla bla bla bla bla bla</span>
+				</span>
+		</article>
