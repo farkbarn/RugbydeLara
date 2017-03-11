6481827ff8fb8321e32a3c78551d25021514a30d
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 17:07:31 -0400

    add new zise image to destaque

--- functions.php
+++ functions.php
@@ -5,8 +5,7 @@ add_theme_support('post-thumbnails');
 set_post_thumbnail_size( 'thumbnail', 175,135, array('center','top') );
 set_post_thumbnail_size( 'medium', 375,270, array('center','top') );
 set_post_thumbnail_size( 'large', 780,535, array('center','top') );
-
-
+add_image_size('destaque', 1180, 450, array('center','top'));
 
 function raiz() {echo get_template_directory_uri().'/';}
 
