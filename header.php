79b9c5e903ead3ec2590f5267ebda19723f000e4
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 14:08:25 -0400

    add page header

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ES">
+<head>
+	<title> Rugby de Lara </title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="Description" content ='Rugby de Lara'>
+	<meta name="Author" content="Frank Barrera - @farkbarn">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- bootstrap css-->
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/'; ?>bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/'; ?>bootstrap/dist/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri().'/'; ?>css/layout.css">
+	<!-- icon -->
+	<link rel="apple-touch-icon" sizes="57x57" href="icon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="icon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="icon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="icon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="icon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="icon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="icon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="icon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="icon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="icon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="icon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="icon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="icon/favicon-16x16.png">
+	<link rel="manifest" href="icon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="icon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+</head>
