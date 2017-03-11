29c37737c473eac992688fbc0730c971e7b62b4a
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 16:57:29 -0400

    create file cabezal.php

---
+++ cabezal.php
@@ -0,0 +1,19 @@
+<body>
+<section class="all">
+	<section class="contenido">
+		<aside class="ads">
+			<img src="http://ximg.es/1180x180/000/fff.jpg" alt="">
+		</aside>
+		<header id="header" class="header">
+			<section class="logo">
+				<span class="logo">Rugby</span> <span class="lara">de lara</span>
+			</section>
+			<section class="rrss">
+				<ul class="ulrrss">
+				    <li class="fb"><a href="#" title=""><span class="icon-facebook"></span></a></li>
+				    <li class="tw"><a href="#" title=""><span class="icon-twitter"></span></a></li>
+				    <li class="ig"><a href="#" title=""><span class="icon-instagram"></span></a></li>
+				    <li class="yt"><a href="#" title=""><span class="icon-youtube"></span></a></li>
+				</ul>
+			</section>
+		</header><!-- /header -->
