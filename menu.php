2d1de63984b168e3aa9634662bccc88f70f40e0b
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 14:07:41 -0400

    add menu item

---
+++ menu.php
@@ -0,0 +1,10 @@
+		<nav class="menumain menu navbar ">
+		    <ul class="nav navbar-nav navbar-center">
+		        <li><a href="#">inicio</a></li><!--
+		     --><li><a href="#">noticias</a></li><!--
+		     --><li><a href="#">equipos</a></li><!--
+		     --><li><a href="#">coaching</a></li><!--
+		     --><li><a href="#">galería</a></li><!--
+		     --><li><a href="#">contacto</a></li>
+		    </ul>
+		</nav>
