0ae33bb303700875c5d0e527992fa6b5e556af66
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 14:08:46 -0400

    add page footer

---
+++ footer.php
@@ -0,0 +1,23 @@
+		<footer>
+			<ul class="footer">
+				<li class="cont">
+					<h2>Contenido</h2>
+					<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
+					<h3>contenido2</h3>
+				</li>
+				<li class="cont">
+					<h2>Contenido</h2>
+					<p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
+					<h3>contenido2</h3>
+				</li>
+			    <li class="logo"><p class="logo"><span class="rugby">Rugby</span> de lara </p></li>
+			</ul>
+		</footer>
+
+	</section>
+</section>
+</body>
+	<!-- bootstrap js -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> <!-- add jq to bootstrap -->
+	<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+</html>
