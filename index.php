7dd8c8352e500aaa01ce934cf61bf267bae9c2bc
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 16:57:49 -0400

    update file

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
-<?php
-get_header();
-?>
-
-<body>
-<section class="all">
-	<section class="contenido">
-		<aside class="ads">
-			<img src="http://ximg.es/1180x180/000/fff.jpg" alt="">
-		</aside>
-		<header id="header" class="header">
-			<section class="logo">
-				<span class="logo">Rugby</span> <span class="lara">de lara</span>
-			</section>
-			<section class="rrss">
-				<ul class="ulrrss">
-				    <li class="fb"><a href="#" title=""><span class="icon-facebook"></span></a></li>
-				    <li class="tw"><a href="#" title=""><span class="icon-twitter"></span></a></li>
-				    <li class="ig"><a href="#" title=""><span class="icon-instagram"></span></a></li>
-				    <li class="yt"><a href="#" title=""><span class="icon-youtube"></span></a></li>
-				</ul>
-			</section>
-		</header><!-- /header -->
-		<?php include('index_post.php'); include('menu.php'); ?>
-		
+<?php get_header(); ?>
+		<?php include('cabezal.php'); include('index_post.php'); include('menu.php'); ?>
 		<section class="notas">
 			<ul class="ulnot">
 			    <li class="linot">
