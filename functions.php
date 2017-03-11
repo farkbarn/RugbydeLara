3d4ba93a5a354355bb1ffe3215f31e36b1610c06
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 16:45:40 -0400

    add sizes preset

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php 
+//add imagen destacada
 add_theme_support('post-thumbnails');
+// size para preestablecidos
+set_post_thumbnail_size( 'thumbnail', 175,135, array('center','top') );
+set_post_thumbnail_size( 'medium', 375,270, array('center','top') );
+set_post_thumbnail_size( 'large', 780,535, array('center','top') );
+
+
 
 function raiz() {echo get_template_directory_uri().'/';}
 
