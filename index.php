228f66612b9ab358cf55ec59730bc92b23073701
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-03-11 14:07:11 -0400

    add index page

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+get_header();
+?>
+
+<body>
+<section class="all">
+	<section class="contenido">
+		<aside class="ads">
+			<img src="http://ximg.es/1180x180/000/fff.jpg" alt="">
+		</aside>
+		<header id="header" class="header">
+			<section class="logo">
+				<span class="logo">Rugby</span> <span class="lara">de lara</span>
+			</section>
+			<section class="rrss">
+				<ul class="ulrrss">
+				    <li class="fb"><a href="#" title=""><span class="icon-facebook"></span></a></li>
+				    <li class="tw"><a href="#" title=""><span class="icon-twitter"></span></a></li>
+				    <li class="ig"><a href="#" title=""><span class="icon-instagram"></span></a></li>
+				    <li class="yt"><a href="#" title=""><span class="icon-youtube"></span></a></li>
+				</ul>
+			</section>
+		</header><!-- /header -->
+		<?php include('index_post.php'); include('menu.php'); ?>
+		
+		<section class="notas">
+			<ul class="ulnot">
+			    <li class="linot">
+			    	<article class="art">
+			    		<img src="http://ximg.es/375x270/000/fff.jpg" alt="">
+		    			<h1>Titulo de nota</h1>
+		    			<p>Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español Contenido aquí, contenido aquí". Estos textos hacen parecerlo un españCon- tenido aquí,  ontenido aquí". Estos textos hacen parecerlo un español Contenido aquí, col</p>
+		    			<p> texto 2</p>
+		    		</article>
+		    	</li>
+
+			    <li class="linot">
+			    	<article class="art">
+			    		<img src="http://ximg.es/375x270/000/fff.jpg" alt="">
+		    			<h1>Titulo de nota</h1>
+		    			<p>Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español Contenido aquí, contenido aquí". Estos textos hacen parecerlo un españCon- tenido aquí,  ontenido aquí". Estos textos hacen parecerlo un español Contenido aquí, col</p>
+						<p> texto 2</p>
+		    		</article>
+		    	</li>
+
+			    <li class="linot">
+			    	<article class="art">
+			    		<img src="http://ximg.es/375x270/000/fff.jpg" alt="">
+		    			<h1>Titulo de nota</h1>
+		    			<p>Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español Contenido aquí, contenido aquí". Estos textos hacen parecerlo un españCon- tenido aquí,  ontenido aquí". Estos textos hacen parecerlo un español Contenido aquí, col</p>
+		    			<p> texto 2</p>
+		    		</article>
+		    	</li>
+
+			    <li class="linot">
+			    	<article class="art">
+			    		<img src="http://ximg.es/375x270/000/fff.jpg" alt="">
+		    			<h1>Titulo de nota</h1>
+		    			<p>Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español Contenido aquí, contenido aquí". Estos textos hacen parecerlo un españCon- tenido aquí,  ontenido aquí". Estos textos hacen parecerlo un español Contenido aquí, col</p>
+		    			<p> texto 2</p>
+		    		</article>
+		    	</li>
+
+			</ul>
+		</section>
+
+		<section class="widget">
+			<ul class="ultrio">
+			    <li class="litrio">
+			    	<article class="art">
+			    		<img src="http://ximg.es/175x135/000/fff.jpg" alt="">
+		    			<h1>Titulo de nota</h1>
+		    			<p>Contenido aquí, contenido aquí".</p>
+		    		</article>
+		    	</li>
+
+			    <li class="litrio">
+			    	<article class="art">
+			    		<img src="http://ximg.es/175x135/000/fff.jpg" alt="">
+		    			<h1>Titulo de nota</h1>
+		    			<p>Contenido aquí, contenido aquí".</p>
+		    		</article>
+		    	</li>
+
+			    <li class="litrio">
+			    	<article class="art">
+			    		<img src="http://ximg.es/175x135/000/fff.jpg" alt="">
+		    			<h1>Titulo de nota</h1>
+		    			<p>Contenido aquí, contenido aquí".</p>
+		    		</article>
+		    	</li>
+			</ul>
+
+			<ul class="uldupla">
+				<li class="livid"></li>
+				<li class="livid"></li>
+			</ul>
+		</section>
+
+
+<?php get_footer();?>
